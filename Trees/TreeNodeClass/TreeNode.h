44c3cb29f872d454efc643eda1425b1ae3c1e780
sparshanurag <30572979+sparshanurag@users.noreply.github.com>
2020-11-01 03:08:36 +0530

    Create TreeNode.h

---
+++ Trees/TreeNodeClass/TreeNode.h
@@ -0,0 +1,16 @@
+#include<vector>
+using namespace std;
+
+template<typename T>
+
+class TreeNode
+{
+    public:
+        T data;
+        vector<TreeNode*> children; // TreeNode<T>*
+        
+        TreeNode(T data)
+        {
+            this->data =data;
+        }
+};
